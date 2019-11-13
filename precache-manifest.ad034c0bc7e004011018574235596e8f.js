0a8fe0e38efd68fba5351dc5f7209be8740bc93e
Justin Nguyen <justinhnguyen88@gmail.com>
2019-11-13 11:58:56 -0800

    Updates

---
+++ precache-manifest.ad034c0bc7e004011018574235596e8f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8bddd5606d3d840f5126c30f0fc1178",
+    "url": "/stocktwits/index.html"
+  },
+  {
+    "revision": "3930e704ad28cebdee94",
+    "url": "/stocktwits/static/js/2.7ad1674e.chunk.js"
+  },
+  {
+    "revision": "26d127cc6f7bba15e096",
+    "url": "/stocktwits/static/js/main.15b31708.chunk.js"
+  },
+  {
+    "revision": "6898f91485c7fe79ce3f",
+    "url": "/stocktwits/static/js/runtime-main.730c1195.js"
+  }
+]);
